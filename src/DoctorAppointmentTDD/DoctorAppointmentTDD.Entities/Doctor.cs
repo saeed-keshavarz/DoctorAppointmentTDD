50be8443745b8cc1b4994ac2de0c421f5b919635
Saeed Keshavarz <saeed.keshavarzi@outlook.com>
2022-04-26 11:54:36 +0430

    Added migration layer to project

--- src/DoctorAppointmentTDD/DoctorAppointmentTDD.Entities/Doctor.cs
+++ src/DoctorAppointmentTDD/DoctorAppointmentTDD.Entities/Doctor.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace DoctorAppointmentTDD.Entities
 {
-    internal class Doctor
+    public class Doctor : Person
     {
+        public Doctor()
+        {
+            Appointments = new List<Appointment>();
+        }
+        public string Field { get; set; }
+        public List<Appointment> Appointments { get; set; }
     }
 }
---
+++ src/DoctorAppointmentTDD/DoctorAppointmentTDD.Migrations/_202204261133_InitialDataBase.cs
@@ -0,0 +1,59 @@
+﻿using FluentMigrator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoctorAppointmentTDD.Migrations
+{
+    [Migration(202204261133)]
+    public class _202204261133_InitialDataBase : Migration
+    {
+        public override void Up()
+        {
+            CreateDoctor();
+            CreatePatient();
+            CreateAppointment();
+        }
+
+        public override void Down()
+        {
+            Delete.Table("Appointments");
+            Delete.Table("Patients");
+            Delete.Table("Doctors");
+        }
+
+        private void CreateAppointment()
+        {
+            Create.Table("Appointments")
+                            .WithColumn("Id").AsInt32().Identity().PrimaryKey()
+                            .WithColumn("Date").AsDate()
+                            .WithColumn("DoctorId").AsInt32().NotNullable()
+                            .ForeignKey("FK_Doctors_Appointments", "Doctors", "Id")
+                            .WithColumn("PatientId").AsInt32().NotNullable()
+                            .ForeignKey("FK_Patients_Appointments", "Patients", "Id");
+        }
+
+        private void CreatePatient()
+        {
+            Create.Table("Patients")
+                             .WithColumn("Id").AsInt32().PrimaryKey().Identity()
+                             .WithColumn("FirstName").AsString(50).NotNullable()
+                             .WithColumn("LastName").AsString(50).NotNullable()
+                             .WithColumn("NationalCode").AsString(10).NotNullable();
+        }
+
+        private void CreateDoctor()
+        {
+            Create.Table("Doctors")
+                             .WithColumn("Id").AsInt32().PrimaryKey().Identity()
+                             .WithColumn("FirstName").AsString(50).NotNullable()
+                             .WithColumn("LastName").AsString(50).NotNullable()
+                             .WithColumn("NationalCode").AsString(10).NotNullable()
+                             .WithColumn("Field").AsString(50).NotNullable();
+
+        }
+
+    }
+}
