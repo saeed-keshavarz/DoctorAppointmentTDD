df197bd153a49d0746e6dc774f6a602f2c814761
Saeed Keshavarz <saeed.keshavarzi@outlook.com>
2022-04-27 14:55:04 +0430

    added Xunit test class for appointment in xunit test layer

---
+++ src/DoctorAppointmentTDD.Services.Test/Appointments/AppointmentServiceTest.cs
@@ -0,0 +1,35 @@
+﻿using DoctorAppointmentTDD.Infrastructur.Test;
+using DoctorAppointmentTDD.Persistence.EF;
+using DoctorAppointmentTDD.Persistence.EF.Appointments;
+using DoctorAppointmentTDD.Service.Appointments;
+using DoctorAppointmentTDD.Service.Appointments.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DoctorAppointmentTDD.Services.Test.Appointments
+{
+    public class AppointmentServiceTest
+    {
+        private readonly ApplicationDbContext _dataContext;
+        private readonly EFUnitOfWork _unitOfWork;
+        private readonly AppointmentRepository _repository;
+        private readonly AppointmentService _sut;
+
+        public AppointmentServiceTest()
+        {
+            _dataContext =
+                new EFInMemoryDatabase()
+                .CreateDataContext<ApplicationDbContext>();
+
+            _unitOfWork = new EFUnitOfWork(_dataContext);
+            _repository = new EFAppointmentRepository(_dataContext);
+            _sut = new AppointmentAppService(_repository, _unitOfWork);
+        }
+
+
+    }
+}
