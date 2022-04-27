2c26b640aed3adbc0fd0b0fc7ec0cd7dbe933748
Saeed Keshavarz <saeed.keshavarzi@outlook.com>
2022-04-27 15:18:04 +0430

    added test Add  for appointment in xunit test layer

--- src/DoctorAppointmentTDD.Services.Test/Appointments/AppointmentServiceTest.cs
+++ src/DoctorAppointmentTDD.Services.Test/Appointments/AppointmentServiceTest.cs
@@ -1,8 +1,12 @@
-﻿using DoctorAppointmentTDD.Infrastructur.Test;
+﻿using DoctorAppointmentTDD.Entities;
+using DoctorAppointmentTDD.Infrastructur.Test;
 using DoctorAppointmentTDD.Persistence.EF;
 using DoctorAppointmentTDD.Persistence.EF.Appointments;
 using DoctorAppointmentTDD.Service.Appointments;
 using DoctorAppointmentTDD.Service.Appointments.Contracts;
+using DoctorAppointmentTDD.Test.Tools.Doctors;
+using DoctorAppointmentTDD.Test.Tools.Patients;
+using FluentAssertions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -30,6 +34,29 @@ namespace DoctorAppointmentTDD.Services.Test.Appointments
             _sut = new AppointmentAppService(_repository, _unitOfWork);
         }
 
+        [Fact]
+        public void Add_add_appointment_properly()
+        {
+            var doctor = DoctorFactory.CreateDoctor("2380132933");
+            _dataContext.Manipulate(_ => _.Add(doctor));
+
+            Patient patient = PatientFactory.CreatePatient("2380257515");
+            _dataContext.Manipulate(_ => _.Add(patient));
+
+            var dto = new AddAppointmentDto()
+            {
+                DoctorId = doctor.Id,
+                PatientId = patient.Id,
+                Date = new DateTime(2022, 04, 27)
+            };
+
+            _sut.Add(dto);
+
+            _dataContext.Appointments.Should().Contain(_ => _.DoctorId == dto.DoctorId &&
+            _.PatientId == dto.PatientId &&
+            _.Date == dto.Date);
+
+        }
 
     }
 }
